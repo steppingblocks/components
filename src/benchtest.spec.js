3902a00036aba91bc52f4d7ef8e91361c2102e49
Mitch Masia <mjmasia@gmail.com>
2019-05-30 16:18:43 -0500

    test(tests): fixes benchtest to pass
    Removes react-testing-library as runtime import causing test failure

--- src/benchtest.spec.js
+++ src/benchtest.spec.js
@@ -1,7 +1,3 @@
-import { cleanup } from 'react-testing-library'
-
-afterEach(cleanup)
-
 describe('passer', () => {
   it('this test will always pass as a benchtest', () => {
     expect(true).toBeTruthy()
