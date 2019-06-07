66af3b3ab025a6ec7a3696a70dcc32f50f009416
Mitch Masia <mjmasia@gmail.com>
2019-06-06 21:35:20 -0500

    fix(build): reduces bundle size by 66 percent

--- build.js
+++ build.js
@@ -1,38 +1,10 @@
-const path = require('path')
-const _fp = require('lodash/fp')
 const Bundler = require('parcel-bundler')
 
-// const isProduction = process.env.NODE_ENV === 'production'
-
-const paths = [
-  'hocs/withTheme',
-  'ui/layout/CenteredLayout',
-  'ui/layout/ConfirmModal',
-  'ui/touchables/CustomButton',
-  'ui/forms/inputs/EmailInput',
-  'ui/forms/GenericForm',
-  'ui/typography/ImportantStatistic',
-  'ui/forms/LoginForm',
-  'ui/layout/MultiColumnLayout',
-  'ui/forms/inputs/PasswordInput',
-  'ui/layout/UnauthenticatedHeader',
-  'ui/layout/UnauthenticatedLayout'
-]
-
-const getEntries = _fp.pipe(
-  _fp.map(start => `${start}/index.js`),
-  _fp.map(file => path.resolve(__dirname, './src', file))
-)
-
-const entries = getEntries(paths)
-
-console.log(entries)
-
-new Bundler(entries, {
+new Bundler('./src/index.js', {
   outDir: 'dist',
   target: 'browser',
   cache: true,
-  sourceMaps: false,
+  sourceMaps: true,
   minify: true,
   watch: false
 }).bundle()
