c010aa4fb44f509c9b2d399906ebcf78ec5ce21a
Mitch Masia <mjmasia@gmail.com>
2019-07-07 12:18:55 -0500

    refactor(filters): moves filters to use id indexes

--- dist/chunk-560c0132.js
+++
@@ -1,6 +0,0 @@
-import { b as button } from './chunk-0626274a.js';
-
-
-
-export { button as r };
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiY2h1bmstNTYwYzAxMzIuanMiLCJzb3VyY2VzIjpbXSwic291cmNlc0NvbnRlbnQiOltdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOzs7OyJ9
